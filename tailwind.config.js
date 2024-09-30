7aa3a2fb050efb3fad14c1876beb76e51ca7412a
w0serr <vzn.sergey@gmail.com>
2024-09-30 23:46:39 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Gilroy', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
